9e10799b9b56b99971fcd9e7415467e191d7426d
Wyatt Joseph Cassiotis <wyatt.cassiotis@gmail.com>
2025-07-31 18:36:44 -0400

    Base of site

---
+++ league-tracker/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('Champions');
+
+  return (
+    <div className="app-container">
+      <header>
+        <div>
+          <nav>
+            <button
+              onClick={() => setCurrentPage('Champions')}
+              className={currentPage === 'Champions' ? 'active' : ''}
+            >
+              Champions
+            </button>
+            <button
+              onClick={() => setCurrentPage('Tracker')}
+              className={currentPage === 'Tracker' ? 'active' : ''}
+            >
+              Tracker
+            </button>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        {/* Content will go here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
